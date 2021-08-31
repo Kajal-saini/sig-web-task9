4c338459c0c04b211878cbd23d33df3e2c7b48a8
Kajal saini <200301104@ipec.org.in>
2021-08-31 12:46:00 +0530

    Task9

---
+++ nodeCh/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+//fs.writeFileSync("Kajal/bio.txt","my name is Kajal Saini");
+//fs.appendFileSync("Kajal/bio.txt","Good Morning :)");
+//const data = fs.readFileSync("Kajal/bio.txt", "utf8");
+//console.log(data);
+//fs.renameSync("Kajal/bio.txt","Kajal/myBio.txt");
+//fs.unlinkSync("Kajal/myBio.txt");
+//fs.rmdirSync(kajal);
